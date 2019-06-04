74b8b33ca49199343abafdea975ff82a37c9d896
UVADave <dpg3k@virginia.edu>
2019-06-04 08:41:02 -0400

    Temp location of build assets

---
+++ package/scripts/entry.sh
@@ -0,0 +1,8 @@
+# run application
+
+# run from here, since application expects json template in templates/
+cd bin; ./v4search -redis_host $V4_SEARCH_REDIS_HOST -redis_pass $V4_SEARCH_REDIS_PASS -redis_prefix $V4_SEARCH_REDIS_PREFIX
+
+#
+# end of file
+#
