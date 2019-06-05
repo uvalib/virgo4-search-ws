e66bcde079d282ef8abf9efac02d94e139c97906
UVADave <dpg3k@virginia.edu>
2019-06-05 09:23:56 -0400

    Dont pass the redis DB password if one is not defined

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,7 +1,12 @@
 # run application
 
+PWD_OPTION=""
+if [ -n "$V4_SEARCH_REDIS_PASS" ]; then
+   PWD_OPTION="-redis_pass $V4_SEARCH_REDIS_PASS"
+fi
+
 # run from here, since application expects json template in templates/
-cd bin; ./v4search -redis_host $V4_SEARCH_REDIS_HOST -redis_pass $V4_SEARCH_REDIS_PASS -redis_prefix $V4_SEARCH_REDIS_PREFIX -redis_db $V4_SEARCH_REDIS_DB
+cd bin; ./v4search -redis_host $V4_SEARCH_REDIS_HOST -redis_port $V4_SEARCH_REDIS_PORT -redis_prefix $V4_SEARCH_REDIS_PREFIX -redis_db $V4_SEARCH_REDIS_DB $PWD_OPTION
 
 #
 # end of file
