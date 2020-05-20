4c38c0f9b55f375dd5cf5165547410c392ce2b46
Dave Goldstein <dpg3k@virginia.edu>
2020-05-20 07:56:54 -0400

    Change database environment names for consistency

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,7 +1,7 @@
 # run application
 
 # run from here
-cd bin; ./v4search -dbhost $V4_DB_HOST -dbport $V4_DB_PORT -dbname $V4_DB_NAME -dbuser $V4_DB_USER -dbpass $V4_DB_PASS -suggestor $V4_SUGGESTOR  -jwtkey $V4_JWT_KEY
+cd bin; ./v4search -dbhost $DBHOST -dbport $DBPORT -dbname $DBNAME -dbuser $DBUSER -dbpass $DBPASS -suggestor $V4_SUGGESTOR -jwtkey $V4_JWT_KEY
 
 #
 # end of file
